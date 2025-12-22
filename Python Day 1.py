989999d53f5c6abda39cf9b49a46804d1b536bf7
Aaisha Jha <jhaaaisha78@gmail.com>
2025-12-22 12:50:45 +0545

    Add personal information collection program
    This program collects basic personal information and prints it out in a formatted way.

---
+++ Python Day 1.py
@@ -0,0 +1,19 @@
+name=input("Enter your name: ")
+print("Hello, my name is " + name)
+
+age = int(input("Enter your age: "))
+print("I am " + str(age) + " years old.")
+
+city=input("Enter your city: ")
+print("I live in " + city + ".")
+
+country=input("Enter your country: ")
+print("I am from " + country + ".")
+
+hobby=input("Enter your hobby: ")
+print("My hobby is " + hobby + ".")
+
+# This program collects basic personal information and prints it out in a formatted way.
+
+print(type(name))
+print(type(age))
