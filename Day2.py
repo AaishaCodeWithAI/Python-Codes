795ff89ece873718574c075b1a8e8a61e15d2faa
Aaisha Jha <jhaaaisha78@gmail.com>
2025-12-22 12:59:14 +0545

    Implement basic arithmetic operations in Day2.py
    This program performs basic arithmetic operations on two numbers and prints the results.

---
+++ Day2.py
@@ -0,0 +1,27 @@
+# Arithmetic operations
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+sum_result = number1 + number2
+print("The sum of " + str(number1) + " and " + str(number2) + " is " + str(sum_result) + ".")
+
+subtraction = number1 - number2
+print("The subtraction of " + str(number1) + " and " + str(number2) + " is " + str(subtraction) + ".")
+
+multiplication = number1 * number2
+print("The multiplication of " + str(number1) + " and " + str(number2) + " is " + str(multiplication) + ".")
+
+division = number1 / number2
+print("The division of " + str(number1) + " and " + str(number2) + " is " + str(division) + ".")
+
+floor_division = number1 // number2
+print("The floor division of " + str(number1) + " and " + str(number2) + " is " + str(floor_division) + ".")
+
+remainder = number1 % number2
+print("The remainder of " + str(number1) + " and " + str(number2) + " is " + str(remainder) + ".")
+
+power = number1 ** number2
+print("The power of " + str(number1) + " and " + str(number2) + " is " + str(power) + ".")
+
+# This program performs basic arithmetic operations on two numbers and prints the results.
+
