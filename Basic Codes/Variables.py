fec3f1b1e298b639fc50e173610a8137428472c1
Aaisha Jha <jhaaaisha78@gmail.com>
2026-01-07 14:05:23 +0545

    Add Variables.py to demonstrate variable usage

---
+++ Basic Codes/Variables.py
@@ -0,0 +1,13 @@
+# Variables store data in a program
+# Variables have names and hold values (data) of different types
+
+name = "Alex"      # String: text
+age = 20           # Integer: whole number
+height = 5.6       # Float: decimal number
+is_student = True  # Boolean: True or False
+
+# Print variable values
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
